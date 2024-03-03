123f44e0e2f5c40069ec16ebdacee3e16a084089
unknown <syafiqurohman7@gmail.com>
2024-03-03 14:31:58 +0700

    first commit

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'ooucholive | Personal Portfolio Website Template';
+export const SITE_DESCRIPTION = 'Junior Web Developer dan desainer grafis dengan hasrat dalam menciptakan pengalaman digital yang menarik. Saya juga menjalankan bisnis kecil sebagai penjual laptop. Ayo berkolaborasi dan ciptakan sesuatu yang luar biasa bersama! it useful!';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
